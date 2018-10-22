c923bacf86aacb56e9f170300ba20b75cc47f871
Krzysztof Langner <klangner@gmail.com>
2018-10-22 15:07:43 +0200

    End of chapter 3

--- src/clojure_sandbox/brave3.clj
+++ src/clojure_sandbox/brave3.clj
@@ -29,3 +29,22 @@
 (map (fn [name] (str "Hi " name)) ["Ala" "Ola"])
 ;; or
 (map #(str "Hi " %) ["Ala" "Ola"])
+
+;; Hobbit
+
+(def asym-hobbit-parts [{:name "head" :size 3}
+                        {:name "left-eye" :size 1}
+                        {:name "left-ear" :size 1}
+                        {:name "mouth" :size 1}])
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symetrize-body-parts
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
